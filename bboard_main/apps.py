86cb2f30819924b530dc8cbccd189b601b87967c
Savely <savely.domnikov@yandex.ru>
2022-10-29 00:42:30 +0300

    Created app bboard_main, added base view and main root

---
+++ bboard_main/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class BboardMainConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "bboard_main"
+    verbose_name = 'Доска объявлений'
